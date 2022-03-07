305484a449223b1988c8ca34f007823d1d644afc
aurélie <adr.31150@gmail.com>
2022-03-07 14:11:42 +0100

    added layout

--- src/view/layout.php
+++ src/view/layout.php
@@ -1,25 +1,84 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>CRM</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/one-page-wonder.min.css" rel="stylesheet">
 
 </head>
+
 <body>
 
-<header>
-    <h1>Header</h1>
-</header>
-<hr>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">CRM</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a href="#" class="pull-left"><img src="/images/Logo-abi.png"></a>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="registration.php">Actualités</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Nous contacter</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Qui sommes nous ?</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin/">Se Connecter</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-<?= $content ?>
+  <header class="masthead text-center text-white">
+    <div class="masthead-content">
+      <div class="container">
+        <h1 class="masthead-heading mb-0">CRM</h1>
+        <h2 class="masthead-subheading mb-0">ABI</h2>
+        <a href="registration.php" class="btn btn-primary btn-xl rounded-pill mt-5">Se Connecter</a>
+      </div>
+    </div>
+    <div class="bg-circle-1 bg-circle"></div>
+    <div class="bg-circle-2 bg-circle"></div>
+    <div class="bg-circle-3 bg-circle"></div>
+    <div class="bg-circle-4 bg-circle"></div>
+  </header>
+<main class="container">
+    <?= $content ?> 
+</main>
 
-<footer>
-    <h1>footer</h1>
-</footer>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-black">
+    <div class="container">
+      <p class="m-0 text-center text-white small">Copyright &copy; CRM 2022 Groupe 2</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap  JavaScript -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
