3d4c8b3143055c43f3c97f62cac3b75a8a275090
osdli <lia.osadta@gmail.com>
2022-03-08 10:43:07 +0100

    création page login création router vers la page login.php création fonction page login.php

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Debogger
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+
+//  organiser la partie configuration
+$config = parse_ini_file(__DIR__ . "/../config.ini");
+define('BASE_URI', $config['base_uri']);
+
+
+
+// Router
+use App\core\Router;
+
+$router = new Router();
+/************ Routes *************/
+$router->register('/', '\App\controller\DefaultController::accueil');
+$router->register('/contact', '\App\controller\DefaultController::contact');
+$router->register('/about', '\App\controller\DefaultController::about');
+$router->register('/form', '\App\controller\DefaultController::form');
+$router->register('/form', '\App\controller\DefaultController::login');
+/************ /Routes *************/
+$router->run();
+
+
