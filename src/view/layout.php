d8dc8b54b4f726f0dcaadd08cd0b730a6bf49825
Andy <96415472+ydalosa@users.noreply.github.com>
2022-03-09 13:27:52 +0100

    Update layout.php

--- src/view/layout.php
+++ src/view/layout.php
@@ -23,13 +23,15 @@
 <body>
 
   <!-- Navigation -->
+ 
   <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
       <a class="navbar-brand" href="#">CRM</a>
+      <img id='pull-left' src="./public/assets/images/Logo-abi.png"></img>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-       <img id='pull-left' src="./public/assets/images/Logo-abi.png"></img>
+     
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
