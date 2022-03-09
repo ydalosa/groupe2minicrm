f53b57a3abaff91770f0fe4002b8b11bbb4ce7ca
DeepRests <100682756+DeepRests@users.noreply.github.com>
2022-03-09 14:14:52 +0100

    Update Default Controller
    Maintenant la fonction form contient les anciens fichiers insert.php et connexionDb.php directement
    Mise à jour des fonctions internes pour l'envoi de données à la BDD
    Le tout est fonctionnel

--- src/controller/DefaultController.php
+++ src/controller/DefaultController.php
@@ -27,7 +27,47 @@ class DefaultController extends Controller
     }
     public function form(){
         //...
+/* Connexion Database */
 
+    $servername='localhost';
+    $username='root';
+    $password='';
+    $dbname = "groupe2minicrm";
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+      if(!$conn){
+          die('Could not Connect MySql Server:');
+        }
+
+/* Equivalent au fichier config.ini */
+
+
+        /* Insertion des données entrées dans le formulaire, dans la Database */
+        
+        if(isset($_POST['submit']))
+        {    
+             $id_sect = $_POST['id_sect'];
+             $raison_sociale = $_POST['raison_sociale'];
+             $adresse_client = $_POST['adresse_client'];
+             $code_postal_client = $_POST['code_postal_client'];
+             $ville_client = $_POST['ville_client'];
+             $ca = $_POST['ca'];
+             $effectif = $_POST['effectif'];
+             $telephone_client = $_POST['telephone_client'];
+             $type_client = $_POST['type_client'];
+             $nature_client = $_POST['nature_client'];
+             $commentaire_client = $_POST['commentaire_client'];
+             $sql = "INSERT INTO clients (id_sect,raison_sociale,adresse_client,code_postal_client,ville_client,ca,effectif,telephone_client,type_client,nature_client,commentaire_client)
+             VALUES ('$id_sect','$raison_sociale','$adresse_client','$code_postal_client','$ville_client','$ca','$effectif','$telephone_client','$type_client','$nature_client','$commentaire_client')";
+             
+             /* Envoi des données à la BDD */
+        
+             if (mysqli_query($conn, $sql)) {
+                echo "New record has been added successfully !";
+             } else {
+                echo "Error: " . $sql . ":-" . mysqli_error($conn);
+             }
+             mysqli_close($conn);
+        }
         $this->renderView('default/form');
        
     }
